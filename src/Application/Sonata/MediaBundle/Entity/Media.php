3422118b46ff56ca22d89b4f6cd20f8f94aeface
Adrien BRAULT <monsti@gmail.com>
2012-04-19 22:42:23 +0200

    Update Application namespace

--- src/Application/Sonata/MediaBundle/Entity/Media.php
+++ src/Application/Sonata/MediaBundle/Entity/Media.php
@@ -1,12 +1,4 @@
 <?php
-/**
- * This file is part of the <name> project.
- *
- * (c) <yourname> <youremail>
- *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
- */
 
 namespace Application\Sonata\MediaBundle\Entity;
 
@@ -18,11 +10,10 @@ use Sonata\MediaBundle\Entity\BaseMedia as BaseMedia;
  * References :
  *   working with object : http://www.doctrine-project.org/projects/orm/2.0/docs/reference/working-with-objects/en
  *
- * @author <yourname> <youremail>
+ * @author Adrien Brault <adrien.brault@gmail.com>
  */
 class Media extends BaseMedia
 {
-
     /**
      * @var integer $id
      */
